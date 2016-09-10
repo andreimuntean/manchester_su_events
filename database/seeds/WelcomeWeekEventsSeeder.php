7493c87d652f78870a4cccde84fb1211e3ed65a5
Andrei Muntean <andrei.muntean.dev@gmail.com>
2016-09-09 18:41:14 -0700

    Update WelcomeWeekEventsSeeder.php

--- database/seeds/WelcomeWeekEventsSeeder.php
+++ database/seeds/WelcomeWeekEventsSeeder.php
@@ -492,7 +492,7 @@ class WelcomeWeekEventsSeeder extends Seeder
     ]);
 
     factory(App\Event::class, 'welcome_week')->create([
-      "date_start"        =>  "2016-09-24",
+      "date_start"        =>  "2016-09-26",
       "time_start"        =>  "18:00",
       "time_end"          =>  "20:00",
       "title"             =>  "Postgraduate Pizza Evening",
@@ -508,15 +508,6 @@ class WelcomeWeekEventsSeeder extends Seeder
       "location_name"     =>  "Council Chambers, Students' Union",
       "location_address"  =>  "Students' Union",
       "cover"             =>  "Living.At.Home.1600x900.jpg"
-    ]);
-    factory(App\Event::class, 'welcome_week')->create([
-      "date_start"        =>  "2016-09-21",
-      "time_start"        =>  "12:00",
-      "time_end"          =>  "13:30",
-      "title"             =>  "Volunteering Showcase",
-      "location_name"     =>  "Council Chambers, 2nd floor, Students' Union",
-      "location_address"  =>  "Students' Union",
-      "description"       =>  "Activities:<ul><li>Take part in micro-volunteering that will support our work in the local community</li><li>Sign-up to one-off volunteering opportunities with children and adults</li><li>Have a go at quizzes and get to know the issues we tackle in Manchester</li></ul>"
     ]);
   }
 }
